9dafa8d6413d6ba3fed1f29d0d4f46fcac5c0060
jgrynczewski <jgrynczewski@gmail.com>
2023-05-29 18:41:39 +0200

    fix after cr - remove secret key

--- intro/settings.py
+++ intro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!v)vlt$%r86-iyht)1!(0(wx*mqcpl5@(f0dpe)=lt*jk=m8d4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

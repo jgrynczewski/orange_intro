a7bed20d32fd6daf9d25643fc6b4b0167a930e5b
jgrynczewski <jgrynczewski@gmail.com>
2023-06-14 19:22:27 +0200

    give attributes

--- farm.py
+++ farm.py
@@ -1,14 +1,27 @@
 # farm
-
-
 class Cow:
+    # inicjalizator (metoda magiczna)
+    def __init__(self, name):
+        """To tutaj grupuje się wszystkie cechy (atrybuty) klasy"""
+
+        self.group = "Mammals"
+        self.name = name
+
     def speak(self):
         print("Muuuuuuuuuuuuuuuu")
 
+    def eat(self, meal):
+        if meal == 3:
+            print('To nie wiesz, że 3 się nie je, debilu!')
+        else:
+            print(f"Jem {meal}")
 
-c = Cow()
+
+c = Cow("Mućka")
 c.speak()
+print(f"To jest krowa {c.name}")
 
-c2 = Cow()
+c2 = Cow("Milka")
 c2.speak()
 
+c.eat("woda")

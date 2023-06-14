2e778aa95a3e7a8d09e4d57f2edd78320b4b3e20
jgrynczewski <jgrynczewski@gmail.com>
2023-06-14 19:50:29 +0200

    state modification

--- farm.py
+++ farm.py
@@ -1,27 +1,45 @@
 # farm
 class Cow:
     # inicjalizator (metoda magiczna)
-    def __init__(self, name):
+    def __init__(self, name, age):
         """To tutaj grupuje się wszystkie cechy (atrybuty) klasy"""
 
-        self.group = "Mammals"
+        # zestaw wszystkich atrybutów nazywany jest stanem obiektu
         self.name = name
+        self.age = age
+        self.hungry = True
 
+    # metoda czytająca stan obiektu (atrubut name)
     def speak(self):
-        print("Muuuuuuuuuuuuuuuu")
+        print(f"Muuuuuuuuuuuuuuuu, jestem krowa o imieniu {self.name}")
 
+    # metody modyfikujące stan obiektu
     def eat(self, meal):
-        if meal == 3:
-            print('To nie wiesz, że 3 się nie je, debilu!')
+        if self.hungry:
+            if meal == 3:
+                print('To nie wiesz, że 3 się nie je, debilu!')
+            else:
+                print(f"Jem {meal}")
+                self.hungry = False
         else:
-            print(f"Jem {meal}")
+            print("Ha, nie jestem głodna. I co mi zrobisz?")
+
+    def run(self):
+        print("Biegam, biegam, biegam ...")
+        self.hungry = True
+        print("aż zgłodniałam")
+
+    def get_older(self):
+        self.age += 1
 
 
-c = Cow("Mućka")
+c = Cow("Mućka", 2)
 c.speak()
-print(f"To jest krowa {c.name}")
-
-c2 = Cow("Milka")
-c2.speak()
-
+c.eat("trawa")
 c.eat("woda")
+c.run()
+c.eat("woda")
+
+print(f"Krowa {c.name} ma {c.age} lat.")
+c.get_older()
+print(f"Krowa {c.name} ma {c.age} lat.")
